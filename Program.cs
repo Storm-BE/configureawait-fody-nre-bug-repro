d255a8658d1e46d4d391efe9972365a182184afa
David Catteuw <david.catteuw@gmail.com>
2023-05-11 21:01:12 +0200

    Add minimal repro
    This fails with a `System.NullReferenceException` due to ConfigureAwait.Fody

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+string? someString = null;
+await (someString == "anyValue" ? SomeMethodAsync() : SomeMethodAsync());
+Console.WriteLine("Finished successfully!");
+
+static Task SomeMethodAsync()
+{
+    return Task.CompletedTask;
+}
